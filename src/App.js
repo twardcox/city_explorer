aedc955d39c887c307e79f7e13d8f0dba7eb442d
twardcox <t.ward.cox@gmail.com>
2019-07-17 11:31:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Search } from './components/Search';
+
+const Results = props => (
+  <results>
+    <section>
+      <h2>{props.title}</h2>
+    </section>
+  </results>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment className="App">
+      <header>
+        <h1>City Explorer</h1>
+        <p>Enter a location below to learn about the weather, events, restaurants, movies, and more!</p>
       </header>
-    </div>
+      <main>
+        <Search />
+        <img src="https://staticmapmaker.com/img/google@2x.png" alt="go here" />
+
+        <Results title="Dark Sky API" />
+        <Results title="Yelp API" />
+        <Results title="Meetup API" />
+        <Results title="MovieDB API" />
+        <Results title="Hiking Project API" />
+      </main>
+    </React.Fragment>
   );
 }
 
