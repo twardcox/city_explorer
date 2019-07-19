7e26af79b940c7875a5018652b0984585a9cba01
twardcox <t.ward.cox@gmail.com>
2019-07-19 12:50:34 -0700

    Updates

---
+++ precache-manifest.cbd0ac8180471a0c6c18b42c80289dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb0f9db71d41247aff732c58e0a7dac",
+    "url": "/twardcox/city_explorer/index.html"
+  },
+  {
+    "revision": "f2a5b7e12ff8f2a20a82",
+    "url": "/twardcox/city_explorer/static/css/main.8bfa3740.chunk.css"
+  },
+  {
+    "revision": "5c940c5fb8347a2d993a",
+    "url": "/twardcox/city_explorer/static/js/2.3df0b21e.chunk.js"
+  },
+  {
+    "revision": "f2a5b7e12ff8f2a20a82",
+    "url": "/twardcox/city_explorer/static/js/main.6be6904e.chunk.js"
+  },
+  {
+    "revision": "9028fca19359bffe016c",
+    "url": "/twardcox/city_explorer/static/js/runtime~main.bc00abf7.js"
+  }
+]);
